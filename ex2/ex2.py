067b5d7615592a3f83725840af38c5015d7524da
Faisal Ali <fiasalaly80@yahoo.co.uk>
2022-02-03 21:13:12 +0000

    ex3 commit

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -1,2 +1,4 @@
 # this a comment in python
 print("I am printing hello world again")
+print("`I could have code like this.")
+print("this will run.")
---
+++ ex3/ex3.py
@@ -0,0 +1,16 @@
+print("I will count my chickens:")
+
+print("Hens",25 + 30/6)
+print("Rooster",100 - 25 * 3 % 4)
+
+print("Now i will count eggs:")
+print(3 + 3 + 4 + 5 + 1 -6 % 2 - 1 / 4 + 6 )
+
+print("is it true that 3 + 2 < 5 -7?")
+
+print(3 + 2 < 5)
+
+print("what is 3 + 2", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh,this is my fault")
