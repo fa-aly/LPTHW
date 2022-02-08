2438fe7d48a5c09007e07f0a0aa7b6f3586a3724
Faisal Ali <fiasalaly80@yahoo.co.uk>
2022-02-08 23:32:44 +0000

    ex5 commit

---
+++ ex5/ex5.py
@@ -0,0 +1,18 @@
+my_name = "faisal ali"
+my_age = 40
+my_height = 78 # inches
+my_weight = 108 # kilogram
+my_eyes = "Black"
+my_teeth = "white"
+my_hair = "Black"
+
+print(f"let's talk about {my_name}")
+print(f"he's {my_height} inches tall.")
+print(f"he's{my_weight} very heavy.")
+print(f"he's {my_eyes} eyes and {my_hair} hair.")
+print("Actually i am not too heavy.")
+print(f"his teeth are usually {my_teeth} depending on coffee.")
+
+total = my_age + my_weight + my_height
+print(f"if I add {my_age}, {my_weight}, and {my_height} I get {total}.")
+
