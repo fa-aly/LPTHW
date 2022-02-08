5806eacab4e7ab6d5a7842e85972ed7206a4ce11
Faisal Ali <fiasalaly80@yahoo.co.uk>
2022-02-08 23:42:08 +0000

    ex5 commit

--- ex5/ex5.py
+++ ex5/ex5.py
@@ -1,3 +1,6 @@
+from multiprocessing.spawn import old_main_modules
+
+
 my_name = "faisal ali"
 my_age = 40
 my_height = 78 # inches
@@ -5,6 +8,9 @@ my_weight = 108 # kilogram
 my_eyes = "Black"
 my_teeth = "white"
 my_hair = "Black"
+ab1 = 'Zaid Ali'
+ab_age = 6 years old
+
 
 print(f"let's talk about {my_name}")
 print(f"he's {my_height} inches tall.")
@@ -12,6 +18,8 @@ print(f"he's{my_weight} very heavy.")
 print(f"he's {my_eyes} eyes and {my_hair} hair.")
 print("Actually i am not too heavy.")
 print(f"his teeth are usually {my_teeth} depending on coffee.")
+print(f"let talk about {ab}")
+print(f"he's {ab_age} years old.")
 
 total = my_age + my_weight + my_height
 print(f"if I add {my_age}, {my_weight}, and {my_height} I get {total}.")
