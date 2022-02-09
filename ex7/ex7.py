4673ead5c17f05e73bc83c94886923e4a3a526b4
Faisal Ali <fiasalaly80@yahoo.co.uk>
2022-02-09 22:23:58 +0000

    ex7 commit

---
+++ ex7/ex7.py
@@ -0,0 +1,20 @@
+print("Mary had a litte lamb")
+print("Its fleece was white as {}. ".format('snow'))
+print("And everywhere that mary cent")
+print("." * 10) # what'd that do?
+
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "b"
+end7 = "u"
+end8 = "r"
+end9 = "g"
+end10 = "e"
+end11 = "s"
+
+# watch end = ' ' at the end . try removing it see what happens
+print(end1 + end2 +end3 +end4 + end5 + end6, end='')
+print(end7 + end8 + end9 + end10 + end11)
