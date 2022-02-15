862439cdfcfb5d31895c6cacfd87817adf6cb6b7
Faisal Ali <fiasalaly80@yahoo.co.uk>
2022-02-15 23:24:03 +0000

    ex10 commit

---
+++ ex10/ex10.py
@@ -0,0 +1,15 @@
+tabby_cat = "\tI am tabbed in." 
+persian_cat = "I am split\non a line."
+backslash_cat = "I am \\ a \\ car."
+
+fat_cat = """
+I will do a list
+\t* Cat food
+\t* Fishes
+\t* catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
