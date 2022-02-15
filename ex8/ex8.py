cb4e4da8497e19bfd9a2c118ed7003fe3baed037
Faisal Ali <fiasalaly80@yahoo.co.uk>
2022-02-15 21:56:24 +0000

    ex8 commit

--- ex8/ex8.py
+++ ex8/ex8.py
@@ -1,10 +1,10 @@
-from socket import fromshare
+
 
 
 formatter = "{} {} {} {}"
-print(formatter.formate(1, 2, 3, 4))
-print(formatter.formate("one", "two", "three", "four"))
-print(formatter.formate(True, False, False, True))
-print(formatter.formate(formatter , formatter, formatter, formatter))
-print(formatter.formate("`Try your", "Own tex here", "Maybe a poem", "Or a song about fear"))
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter , formatter, formatter, formatter))
+print(formatter.format("`Try your", "Own tex here", "Maybe a poem", "Or a song about fear"))
  
