8855df56038957143f8719aa9ae2027f087dc7c8
Faisal Ali <fiasalaly80@yahoo.co.uk>
2022-02-16 23:02:43 +0000

    ex11 commit

---
+++ ex11/ex11.py
@@ -0,0 +1,9 @@
+print("how old are you?" , end='')
+age = input()
+print("how tall are you?" , end='')
+height = input()
+print("how much do you weigh?" , end='')
+weigh =input()
+
+print(f"So, you are {age} old, {height} tall and {weight} heeavy.")
+
