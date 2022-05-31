8915b40ef3521c87d574719c1d0736adabdf2a08
Faisal Ali <fiasalaly80@yahoo.co.uk>
2022-05-31 23:11:25 +0100

    ex13 commit

---
+++ ex13/ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third =argv
+
+print("the script is called:",script)
+print("Your variable is first:",first)
+print("your second variable is:",second)
+print("your third variable is:", third)
